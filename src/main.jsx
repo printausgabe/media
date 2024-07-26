8c751ddf0c31d1fd3adbe1cc96423e96a3bc0f0e
ralf <items@freenet.de>
2024-07-26 12:34:21 +0200

    Initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
+import store from './store'
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
